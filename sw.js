27e74fbd8e3b824522b43e0ad9401283bfab12b1
Glitch (teakadai) <none>
2020-01-06 10:58:18 +0000

    🌇🔑 Updated with Glitch

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@ const offlineHTML = `
   <head>
       <meta charset="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <title>My site | Offline </title>
+      <title>TeaKadai | Offline </title>
       <meta name="description" content="">
       <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
