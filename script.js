7d311d138f5654a97bb3994e3a4efb1b53b26305
Glitch (teakadai) <none>
2021-04-07 17:18:56 +0000

    🐡🙎 Updated with Glitch

--- script.js
+++ script.js
@@ -138,7 +138,7 @@ document.documentElement.scrollTop = 0;
   
 var x = document.getElementById("snackbar");
   x.className = "show";
-  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);  
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 1000);  
 }
 
 
